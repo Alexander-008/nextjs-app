c1c62899d9b511b80d4d855d8391bfbf6f54cb95
LAPTOP-VGN7PV3T\20958 <2095893616@qq.com>
2025-06-21 10:39:41 +0800

    数据请求页面完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
-
-export default function Page() {
+import { revalidatePath } from 'next/cache'
+import { neon } from '@neondatabase/serverless'
+const sql = neon(`${process.env.DATABASE_URL}`)
+export default async function Page() {
+  const result = await sql`SELECT * FROM users`;
+  async function createAction(formData: FormData) {
+    'use server'
+    const userName = formData.get('userName')
+    const userAge = formData.get('userAge')
+    const address = formData.get('address')
+    await sql`INSERT INTO users (userName,userAge,address) VALUES (${userName}, ${userAge},${address})`;
+    revalidatePath('/')
+  }
   return (
-     <div>
-        NextJs App
-     </div>
-  );
+    <div>
+      hello page 01
+      <div>
+        <form action={createAction}>
+          <div>
+            用户名：<input className="border" type="text" name="userName" />
+          </div>
+          <div>
+            年龄：<input className="border" type="text" name="userAge" />
+          </div>
+           <div>
+            地址：<input className="border" type="text" name="address" />
+          </div>
+          <button type="submit">注册</button>
+        </form>
+      </div>
+      <ul>
+        { result.map(item => <li key={item.id}>{item.username}, {item.password},{item.address}</li>) }
+      </ul>
+    </div>
+  )
 }
